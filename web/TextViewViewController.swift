03ce628f5858a720d0741e32a9519821cb3b5d7c
Kevin He <3057469318@qq.com>
2020-02-01 18:45:54 +0800

    add note vc

---
+++ web/TextViewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TextViewViewController.swift
+//  web
+//
+//  Created by kevinhe on 2020/2/1.
+//  Copyright © 2020 kevinhe. All rights reserved.
+//
+
+import UIKit
+
+class TextViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
