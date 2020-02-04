19605d2743a8f8058dcd690df1c8be247cbb0cfe
Kevin He <3057469318@qq.com>
2020-02-04 21:28:09 +0800

    change textView text size ,  14 to 17

--- web/PagesTableViewController.swift
+++ web/PagesTableViewController.swift
@@ -117,14 +117,6 @@ class PagesTableViewController: UITableViewController {
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showNotes" {
-            if let nvc = segue.destination as? NotesTableViewController {
-                notes = ["1", "2"]
-                if let notes = defaults.array(forKey: key) as? [String] {
-                    nvc.notes = notes
-                }
-            }
-        }
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
         
